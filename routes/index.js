62d63db620460162139588ec228fe6e9aa0dab0a
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-16 12:50:54 -0600

    task 2

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+import AppController from '../controllers/AppController';
+
+const express = require('express');
+
+const router = (app) => {
+    const route = express.Router();
+    app.use(express.json());
+    app.use('/', route);
+
+route.get('/status', ((request, response) => AppController.getStatus(request, response)));
+route.get('/stats', ((request, response) => AppController.getStats(request, response)));
+
+};
+
+export default router;
