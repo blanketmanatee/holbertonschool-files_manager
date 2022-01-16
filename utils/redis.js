8fe5c3968792293698b9d1b0577704a20407ec80
jashjchoi <jashjchoi@gmail.com>
2022-01-16 19:59:34 +0000

    debugging. Still getting false for the 1st function

--- utils/redis.js
+++ utils/redis.js
@@ -1,33 +1,27 @@
-import redis from 'redis';
-
+const redis = require('redis');
 const { promisify } = require('util');
 
 class RedisClient {
-  // Every client must create and connect to redis
   constructor() {
     this.client = redis.createClient();
-    this.client.on('error', (error) => console.error(`Redis client not connected to the server: ${error.message}`));
+    this.client.on('error', (error) => console.log(error));
   }
 
-  // Returns status of client
   isAlive() {
     return this.client.connected;
   }
 
-  // Returns value at key from redis store
   async get(key) {
     const asyncGet = promisify(this.client.get).bind(this.client);
     const value = await asyncGet(key).catch(console.error);
     return value;
   }
 
-  // Sets value into redis store
   async set(key, value, duration) {
     const asyncSet = promisify(this.client.set).bind(this.client);
     await asyncSet(key, value, 'EX', duration).catch(console.error);
   }
 
-  // Deletes value at redis store
   async del(key) {
     const asyncDel = promisify(this.client.del).bind(this.client);
     await asyncDel(key).catch(console.error);
@@ -35,4 +29,4 @@ class RedisClient {
 }
 
 const redisClient = new RedisClient();
-module.exports = redisClient;
+export default redisClient;
