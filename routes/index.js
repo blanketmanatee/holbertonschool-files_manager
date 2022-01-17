1705d1c24981aab4ea74053d8396b2905096046a
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-17 10:44:54 -0600

    adding task 2 and 3 for checker

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+import AppController from '../controllers/AppController';
+
+const express = require('express');
+
+const router = (app) => {
+    const route = express.Router();
+    app.use(express.json());
+    app.use('/', route);
+
+route.get('/status', ((request, response) => AppController.getStatus(request, response)));
+route.get('/stats', ((request, response) => AppController.getStats(request, response)));
+route.post('/users', ((request, response) => UsersControllers.postNew(request, response)));
+route.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
+route.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
+route.get('users/me', ((request, response) => UserController.getMe (request, response)));
+route.post('/files', ((request, response) => FilesController.postUpload (request, response)));
+route.get('/files/:id', ((request, response) => FilesController.getShow (request, response)));
+route.get('/files', ((request, response) => FilesController.getIndex(request, response)));
+route.put('/files/:id/publish', ((request, response) => FilesController.putPublish(request, response)));
+route.put('/files/:id/publish', ((request, response) => FilesController.putUnpublish(request, response)));
+route.get('/files/:id/data', ((request, response) => FilesController.getFile(request, response)));
+
+
+};
+
+export default router;
