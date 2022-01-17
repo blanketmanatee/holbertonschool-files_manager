f1d9cd6e176481f1bb525b5a26da843cf8b63e70
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-17 15:49:05 -0600

    testing route

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
+import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
-const express = require('express');
-
-const router = (app) => {
+function Routing(app) {
     const route = express.Router();
-    app.use(express.json());
     app.use('/', route);
 
-route.get('/status', ((request, response) => AppController.getStatus(request, response)));
+route.get('/status', (request, response) => { 
+    AppController.getStatus(request, response);
+});
 route.get('/stats', ((request, response) => AppController.getStats(request, response)));
-route.post('/users', ((request, response) => UsersControllers.postNew(request, response)));
+route.post('/users', ((request, response) => UsersController.postNew(request, response)));
 route.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
 route.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
 route.get('users/me', ((request, response) => UserController.getMe (request, response)));
