59346bb628bd019c97345bd7ddaa609bf90782b6
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-17 17:10:02 -0600

    corrected index.js

--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,7 @@ route.get('/status', (request, response) => {
 });
 route.get('/stats', ((request, response) => AppController.getStats(request, response)));
 route.post('/users', ((request, response) => UsersController.postNew(request, response)));
-route.get('/connect', ((request, response) => AuthController.getConnect(request, response)));
-route.get('/disconnect', ((request, response) => AuthController.getDisconnect(request, response)));
-route.get('users/me', ((request, response) => UserController.getMe (request, response)));
-route.post('/files', ((request, response) => FilesController.postUpload (request, response)));
-route.get('/files/:id', ((request, response) => FilesController.getShow (request, response)));
-route.get('/files', ((request, response) => FilesController.getIndex(request, response)));
-route.put('/files/:id/publish', ((request, response) => FilesController.putPublish(request, response)));
-route.put('/files/:id/publish', ((request, response) => FilesController.putUnpublish(request, response)));
-route.get('/files/:id/data', ((request, response) => FilesController.getFile(request, response)));
-
 
 };
 
-export default router;
+export default Routing;
