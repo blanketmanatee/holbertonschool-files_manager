47363281b258e759a09a5c2fe8cbe2de2fc37d91
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-17 17:19:33 -0600

    corrected index.js

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 
 function Routing(app) {
-    const route = express.Router();
-    app.use('/', route);
+  const route = express.Router();
+  app.use('/', route);
 
 route.get('/status', (request, response) => { 
-    AppController.getStatus(request, response);
+  AppController.getStatus(request, response);
 });
 route.get('/stats', ((request, response) => AppController.getStats(request, response)));
 route.post('/users', ((request, response) => UsersController.postNew(request, response)));
 
-};
+}
 
 export default Routing;
