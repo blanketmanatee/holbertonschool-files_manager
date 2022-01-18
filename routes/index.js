2235bd1011b68a42549213942d769f95d1ed8d9a
jashjchoi <jashjchoi@gmail.com>
2022-01-18 04:42:45 +0000

    modified index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  console.log('basic endpoint, greeting sent');
   res.send('hello world\n');
 });
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
