54ea0995482225667542affb801c815f4019a720
jashjchoi <jashjchoi@gmail.com>
2022-01-19 21:48:53 +0000

    modified usercontrollers. Returns the correct outputs.

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,24 +1,27 @@
-import DBClient from '../utils/db';
-import redisClient from '../utils/redis';
 import crypto from 'crypto';
+import DBClient from '../utils/db';
+
+const Bull = require('bull');
 
 class UsersController {
   static async postNew(request, response) {
+    const userQueue = new Bull('userQueue');
     const { email, password } = request.body;
-    if (!email)
-      return response.status(400).json({ error: 'Missing email' });
-    if (!password)
-      return response.status(400).json({ error: 'Missing password' });
+    if (!email) return response.status(400).json({ error: 'Missing email' });
+    if (!password) return response.status(400).json({ error: 'Missing password' });
 
     const users = await DBClient.db.collection('users');
     const existUser = await users.find({ email }).toArray();
-    if (existUser.length > 0)
-      return response.status(400).json({ error: 'Already exist' });
+    if (existUser.length > 0) return response.status(400).json({ error: 'Already exist' });
 
     const hashPw = crypto.createHash('SHA1').update(password).digest('hex');
     const createUser = await users.insertOne({ email, password: hashPw });
     const newUser = { id: createUser.insertedId, email };
-    return response.status(201).json(newUser);
+    // return response.status(201).json(newUser);
+
+    userQueue.add({ userId: newUser.id });
+    response.statusCode = 201;
+    return response.json(newUser);
   }
 }
 
