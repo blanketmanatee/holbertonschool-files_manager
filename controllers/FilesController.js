fb90cad017688217a1952b52d793e7a215ecd851
blanketmanatee <ashleyl.welch@gmail.com>
2022-01-20 11:49:06 -0600

    task 5

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,6 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
 import Bull from 'bull';
 import DBClient from '../utils/db';
 import RedisClient from '../utils/redis';
+import { response } from 'express';
+
+const { ObjectId } = require('mongodb');
+const fs = require('fs');
+const mime = require('mime-types');
+const Bull = require('bull')
 
 class FilesController {
     static async postUpload(request, response) {
@@ -24,6 +31,92 @@ class FilesController {
         const fileData = request.body.data;
         if (!fileData && ['file', 'image'].includes(fileType)) return response.status(400).send({ error: 'Missing data' });
 
-        
+        const filesIsPublic = request.body.isPublic || false;
+        let fileParentId = request.body.parentId || 0;
+        fileParentId = fileParentId === '0' ? 0 : fileParentId;
+        if (fileParentId !== 0) {
+            const parentFile = await DBClient.db.collection('files').findOne({ _id: ObjectId(fileParentId) });
+            if (!parentFile) return response.status(400).send({ error: 'Parent not found' });
+            if (!['folder']. includes(parentFile.type)) return response.status(400).send({ error: 'Parent is not a folder' });
+        }
+
+        const fileDataDb = {
+            userId: user._id,
+            name: fileName,
+            type: fileType,
+            isPublic: filesIsPublic,
+            parentId: fileParentId,
+        };
+
+        if (['folder'].includes(fileType)) {
+            await DBClient.db.collection('files').insertOne(fileDataDb);
+            return response.status(201).send ({
+                id: fileDataDb._id,
+                userId: fileDataDb.userId,
+                name: fileDataDb.name,
+                type: fileDataDb.type,
+                isPublic: fileDataDb.isPublic,
+                parentId: fileDataDb.parentId,
+            });
+        }
+
+        const pathDir = process.env.FOLDER_PATH || '/tmp/files_manager';
+        const fileUuid = uuidv4();
+
+        const buff = Buffer.from(fileData, 'base64');
+        const pathFile = `${pathDir}/${fileUuid}`;
+
+        await fs.mkdir(pathDir, { recursive: true }, (error) => {
+            if (error) return response.status(400).send({ error: error.message });
+            return true;
+        });
+
+        await fs.writeFile(pathFIle, buff, (error) => {
+            if (error) return response.status(400).send({ error: error.message });
+            return true;
+        });
+
+        fileDataDb.localPath = pathFile;
+        await DBClient.db.collection('files').insertOne(fileDataDb);
+
+        fileQueue.add({
+            userId: fileDataDb.userId,
+            fileId: fileDataDb._id,
+        });
+
+        return response.status(201).send({
+            id: fileDataDb._id,
+            userId: fileDataDb.userId,
+            name: fileDataDb.name,
+            type: fileDataDb.type,
+            isPublic: fileDataDb.isPublic,
+            parentId: fileDataDb.parentId,
+        });
     }
+
+    static async getShow(req, res){
+        const token = request.header('X-token') || null;
+        if (!token) return response.status(401).send({ error: 'Unauthorized' });
+
+        const redisToken = await RedisClient.get(`auth_${token}`);
+        if (!redisToken) return response.status(401).send({ error: 'Unauthorized' });
+
+        const user = await DBClient.db.collection('users').findOne({ _id: ObjectId(redisToken) });
+        if (!user) return response.status(401).send({ error: 'Unauthorized' });
+
+        const idFile = request.params.id || '';
+
+        const fileDocument = await DBClient.db.collection('files').findOne({ _id: ObjectId(idFile), userId: user._id });
+        if (!fileDocument) return response.status(404).send({ error: 'Not found' });
+
+        return response.send({
+            id: fileDocument._id,
+            userId: fileDocument.userId,
+            name: fileDocument.name,
+            type: fileDocument.type,
+            isPublic: fileDocument.isPublic,
+            parentId: fileDocument.parentId,
+        });
+    }
+    
 }
